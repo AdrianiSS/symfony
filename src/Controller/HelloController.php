50fdf8609b857a1bd223e99e378849c6194768e2
AdrianiSS <adrianzivcic@gmail.com>
2024-05-02 22:43:08 +0200

    bug fix

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -13,17 +13,23 @@ class HelloController extends AbstractController
     private array $messages = [
         "Hello", "Hi", "Bye"
     ];
+
     #[Route('/{limit<\d+>?3}', name: 'app_index')]
     public function index(int $limit): Response
     {
-        return new Response(
-            implode(',', array_slice($this->messages, 0, $limit))
-        );
+        return $this->render('hello/index.html.twig',
+            ['message' => implode(',', array_slice($this->messages, 0, $limit))]);
     }
 
     #[Route('/messages/{id<\d+>}', name: 'app_show_one')]
-    public   function showOne(int $id): Response
+    public function showOne(int $id): Response
     {
-        return new Response($this->messages[$id]);
+        return $this->render(
+            'hello/show_one.html.twig',
+            [
+                'messages' => $this->messages[$id]
+            ]
+        );
+//        return new Response($this->messages[$id]);
     }
 }
