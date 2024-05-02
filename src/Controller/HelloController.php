c0b11e9c702414944d16a8b2ba9e9a26b4c25c85
AdrianiSS <adrianzivcic@gmail.com>
2024-05-02 22:30:17 +0200

    routes

---
+++ src/Controller/HelloController.php
@@ -0,0 +1,29 @@
+<?php
+
+declare(strict_types=1);
+
+namespace App\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\Routing\Attribute\Route;
+
+class HelloController extends AbstractController
+{
+    private array $messages = [
+        "Hello", "Hi", "Bye"
+    ];
+    #[Route('/{limit<\d+>?3}', name: 'app_index')]
+    public function index(int $limit): Response
+    {
+        return new Response(
+            implode(',', array_slice($this->messages, 0, $limit))
+        );
+    }
+
+    #[Route('/messages/{id<\d+>}', name: 'app_show_one')]
+    public   function showOne(int $id): Response
+    {
+        return new Response($this->messages[$id]);
+    }
+}
