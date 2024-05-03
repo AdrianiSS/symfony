150a8ce043f617c9be79189fba1083cd00c2d83e
Adrian Zivcic <zivcic@tssgroup.sk>
2024-05-03 15:16:56 +0200

    Twig control structures

--- src/Controller/HelloController.php
+++ src/Controller/HelloController.php
@@ -17,8 +17,12 @@ class HelloController extends AbstractController
     #[Route('/{limit<\d+>?3}', name: 'app_index')]
     public function index(int $limit): Response
     {
-        return $this->render('hello/index.html.twig',
-            ['message' => implode(',', array_slice($this->messages, 0, $limit))]);
+        return $this->render(
+            'hello/index.html.twig',
+            [
+                'messages' => array_slice($this->messages, 0, $limit)
+            ]
+        );
     }
 
     #[Route('/messages/{id<\d+>}', name: 'app_show_one')]
