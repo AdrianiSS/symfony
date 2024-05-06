794571df53914169ad466ca88882841320343a03
AdrianiSS <adrianzivcic@gmail.com>
2024-05-06 21:50:03 +0200

    add to databse in controller

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -2,6 +2,8 @@
 
 namespace App\Controller;
 
+use App\Entity\MicroPost;
+use App\Repository\MicroPostRepository;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
@@ -9,8 +11,15 @@ use Symfony\Component\Routing\Attribute\Route;
 class MicroPostController extends AbstractController
 {
     #[Route('/micro-post', name: 'app_micro_post')]
-    public function index(): Response
+    public function index(MicroPostRepository $posts): Response
     {
+        $microPost = New MicroPost();
+        $microPost->setTitle('It comes from controller');
+        $microPost->setText('HI!');
+        $microPost->setCreated(new \DateTime());
+
+        $posts->add($microPost, true);
+//        dd($posts->find(4));
         return $this->render('micro_post/index.html.twig', [
             'controller_name' => 'MicroPostController',
         ]);
