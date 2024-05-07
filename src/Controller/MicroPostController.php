7fac8692c49436dfbdd6b74ec5052ce8bc5df8ed
Adrian Zivcic <zivcic@tssgroup.sk>
2024-05-07 09:36:20 +0200

    handle data, add data, get data

--- src/Controller/MicroPostController.php
+++ src/Controller/MicroPostController.php
@@ -8,6 +8,7 @@ use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 use Symfony\Component\Form\Extension\Core\Type\TextareaType;
 use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Attribute\Route;
 
@@ -28,7 +29,7 @@ class MicroPostController extends AbstractController
         ]);
     }
     #[Route('/micro-post/create', name: 'app_micro_post_create', priority: 2)]
-    public function add(): Response
+    public function add(Request $request, MicroPostRepository $posts): Response
     {
         $microPost = new MicroPost();
         $formBuilder = $this->createFormBuilder($microPost);
@@ -37,6 +38,14 @@ class MicroPostController extends AbstractController
             ->add('text', TextareaType::class)
             ->add('Submit', SubmitType::class, ['label' => 'Save'])
             ->getForm();
+
+        $form->handleRequest($request);
+
+        if ($form->isSubmitted() && $form->isValid()) {
+            $post = $form->getData();
+            $post->setCreated(new \DateTime());
+            $posts->add($post, true);
+        }
 
         return $this->render('micro_post/create.html.twig', [
                 'form' => $form,
