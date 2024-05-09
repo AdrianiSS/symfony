254ce57ce8205082b7a09e061526cbaa2d384a4f
Adrian Zivcic <zivcic@tssgroup.sk>
2024-05-09 12:58:40 +0200

    create command

--- src/DataFixtures/AppFixtures.php
+++ src/DataFixtures/AppFixtures.php
@@ -3,13 +3,43 @@
 namespace App\DataFixtures;
 
 use App\Entity\MicroPost;
+use App\Entity\User;
+use App\Repository\UserRepository;
 use Doctrine\Bundle\FixturesBundle\Fixture;
 use Doctrine\Persistence\ObjectManager;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 
 class AppFixtures extends Fixture
 {
+
+    public function __construct(
+        private UserPasswordHasherInterface $userPasswordHasher,
+    ) {
+
+    }
+
     public function load(ObjectManager $manager): void
     {
+        $user1 = new User();
+        $user1->setEmail('test@test.com');
+        $user1->setPassword(
+            $this->userPasswordHasher->hashPassword(
+                $user1,
+                'password'
+            )
+        );
+        $manager->persist($user1);
+
+        $user2 = new User();
+        $user2->setEmail('test2@test.com');
+        $user2->setPassword(
+            $this->userPasswordHasher->hashPassword(
+                $user2,
+                'password2'
+            )
+        );
+        $manager->persist($user2);
+
         $microPost1 = new MicroPost();
         $microPost1->setTitle('Welcome to Slovakia');
         $microPost1->setText('Welcome to Slovakia');
