2ce38d8c3a561e65d56651712ae6ab4bea98c206
Adrian Zivcic <zivcic@tssgroup.sk>
2024-05-14 09:04:31 +0200

    user posts optimizing query

--- src/Entity/MicroPost.php
+++ src/Entity/MicroPost.php
@@ -116,11 +116,9 @@ class MicroPost
 
     public function removeComment(Comment $comment): static
     {
-        if ($this->comments->removeElement($comment)) {
-            // set the owning side to null (unless already changed)
-            if ($comment->getPost() === $this) {
-                $comment->setPost(null);
-            }
+        // set the owning side to null (unless already changed)
+        if ($this->comments->removeElement($comment) && $comment->getPost() === $this) {
+            $comment->setPost(null);
         }
 
         return $this;
